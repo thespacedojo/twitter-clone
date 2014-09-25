73f9b2183fe76966ec79e1d13c6c438684e58457
Josh Owens <joshua.owens@gmail.com>
2014-09-25 08:14:11 -0400

    Get collection helpers in place for username and full name. Finishes #4

--- collections/tweets.js
+++ collections/tweets.js
@@ -1,5 +1,21 @@
 Tweets = new Mongo.Collection('tweets')
 
+Tweets.helpers({
+  username: function() {
+    user = Meteor.users.findOne({_id: this.userId});
+    if (user) {
+      return user.username;
+    }
+  },
+
+  userFullName: function() {
+    user = Meteor.users.findOne({_id: this.userId});
+    if (user && user.profile && user.profile.name) {
+      return user.profile.name;
+    }
+  }
+})
+
 Tweets.before.insert(function (userId, doc) {
   doc.tweetedAt = Date.now();
   doc.userId = userId;
