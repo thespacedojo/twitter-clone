614cd0750438991ca33e6aced5151317b8de0745
Josh Owens <joshua.owens@gmail.com>
2014-09-25 08:03:58 -0400

    Tweak the accounts entry setup to include username and full name. Refs #3

--- client/accountsEntry.js
+++ client/accountsEntry.js
@@ -1,6 +1,13 @@
 Meteor.startup(function () {
   AccountsEntry.config({
     homeRoute: '/',
-    dashboardRoute: '/'
+    dashboardRoute: '/',
+    passwordSignupFields: 'USERNAME_AND_EMAIL',
+    extraSignUpFields: [{
+      field: 'name',
+      label: 'Full Name',
+      type: 'text',
+      required: true 
+    }]
   })
 })
