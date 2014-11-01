dcb49bb0b031e41be7124d97eab97b0067800d7b
Josh Owens <joshua.owens@gmail.com>
2014-11-01 13:33:57 -0400

    Make follow button work. Refs #5

--- client/profile.js
+++ client/profile.js
@@ -10,6 +10,9 @@ Template.profile.helpers({
   joined: function() {
     return moment(this.createdAt).format("MMMM Do, YYYY")
   },
+  following: function() {
+    return _.contains(Meteor.user().profile.followingIds, this._id)
+  },
   newTweets: function() {
     return this.tweets(Session.get('tweetsSeenAt'), true);
   },
@@ -21,5 +24,8 @@ Template.profile.helpers({
 Template.profile.events({
   'click .showTweets': function(event, template) {
     Session.set('tweetsSeenAt', new Date());
+  },
+  'click .follow-me': function(event, template) {
+    Meteor.call('follow', template.data._id);
   }
 });
--- collections/users.js
+++ collections/users.js
@@ -9,3 +9,9 @@ Users.helpers({
     }
   }
 });
+
+Meteor.methods({
+  follow: function(followId) {
+    Users.update(this.userId, {$push: {"profile.followingIds": followId}});
+  }
+})
