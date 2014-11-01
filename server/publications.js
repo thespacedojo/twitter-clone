203f03dadb721b7642339ee851a07786ac1cb78c
Josh Owens <joshua.owens@gmail.com>
2014-11-01 14:16:03 -0400

    Get joins working, along with the start of reactive joins in the comments of the pub. Refs #7

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,24 @@
 Meteor.publish('tweets', function() {
-  return Tweets.find({}, {sort: {tweetedAt: -1}});
+  self = this;
+  cursors = [];
+  userCursor = Users.find({_id: self.userId});
+  user = userCursor.fetch()[0]
+  ids = [];
+  ids.push(user.profile.followingIds);
+  ids.push(self.userId);
+  followingIds = _.flatten(ids);
+
+  // userCursor.observeChanges({
+  //   changed: function(id, fields) {
+  //     // Code that finds added and removed ids, probably using _.difference
+  //     _.each(addedIds, function(addedId)
+  //       self.added('users', addedId, Users.findOne({_id: addedId}, {fields: {username: 1, 'profile.name': 1}));
+  //     }
+  //   }
+  // })
+  cursors.push(Users.find({_id: {$in: followingIds}}, {fields: {username: 1, 'profile.name': 1}}));
+  cursors.push(Tweets.find({userId: {$in: followingIds}}, {sort: {tweetedAt: -1}}));
+  return cursors
 });
 
 Meteor.publish('profile', function(username) {
