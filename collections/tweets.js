a389e0e5e055adfd954ab4b28aa6162d97603273
Josh Owens <joshua.owens@gmail.com>
2014-11-01 11:18:02 -0400

    Get tweet partial setup and mostly working. Refs #4.

---
+++ collections/tweets.js
@@ -0,0 +1,34 @@
+Tweets = new Mongo.Collection('tweets');
+
+Tweets.before.insert(function(userId, doc) {
+  doc.tweetedAt = new Date();
+  doc.userId = userId;
+});
+
+Tweets.helpers({
+  username: function() {
+    user = Meteor.users.findOne({_id: this.userId});
+    if (user) {
+      return user.username;
+    }
+  },
+  userFullName: function() {
+    user = Meteor.users.findOne({_id: this.userId});
+    if (user && user.profile && user.profile.name) {
+      return user.profile.name;
+    }
+  }
+});
+
+Tweets.allow({
+  insert: function(userId, doc) {
+    if (userId)
+      return true;
+  },
+  update: function(userId, doc, fields, modifier) {
+    return false;
+  },
+  remove: function(userId, doc) {
+    return doc.userId === userId;
+  }
+})
