99aa58412b36c34b845b3bbd29286836399ee77d
Josh Owens <joshua.owens@gmail.com>
2014-11-06 11:40:31 -0500

    Get the start of profile hookup. Refs #5 #6

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
 Meteor.publish('tweets', function() {
   return Tweets.find();
 });
+
+Meteor.publish('profile', function(username) {
+  return Users.find({username: username}, {fields: {services: 0, emails: 0}});
+});
+
+Meteor.publish('profileTweets', function(username) {
+  user = Users.findOne({username: username});
+  return Tweets.find({userId: user._id});
+});
