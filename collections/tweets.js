84cf883100090940f348319d00d5fc68f4e5e4ea
Josh Owens <joshua.owens@gmail.com>
2014-11-06 15:38:07 -0500

    Capture mentions. Refs #7

--- collections/tweets.js
+++ collections/tweets.js
@@ -1,8 +1,19 @@
 Tweets = new Mongo.Collection('tweets')
 
+processTweet = function(text) {
+  if (Meteor.isServer) {
+    if (_.contains(text, "@")) {
+      mentions = _.select(text.split(" "), function(string) {return _.contains(string, "@")})
+      usernames = _.map(mentions, function(mention) {return mention.substring(1)})
+      return _.pluck(Users.find({username: {$in: usernames}}).fetch(), "_id")
+    }
+  }
+}
+
 Tweets.before.insert(function(userId, doc) {
   doc.tweetedAt = new Date();
   doc.userId = userId;
+  doc.mentionIds = processTweet(doc.text);
 });
 
 Tweets.helpers({
